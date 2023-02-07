4d23ea9489a78b91318aa94acbf320aaacd5b9e2
nikhilvkth <124304138+nikhilvkth@users.noreply.github.com>
2023-02-07 01:29:02 -0500

    Update simple_linear_regression.R

--- simple_linear_regression.R
+++ simple_linear_regression.R
@@ -1,29 +1,24 @@
 # Simple Linear Regression
 
-# Importing the dataset
+
 dataset = read.csv('Salary_Data.csv')
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
+
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
 
-# Fitting Simple Linear Regression to the Training set
 regressor = lm(formula = Salary ~ YearsExperience,
                data = training_set)
 
-# Predicting the Test set results
+
 y_pred = predict(regressor, newdata = test_set)
 #install.packages('ggplot2')
 #library('ggplot2')
-# Visualising the Training set results
+
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
@@ -34,7 +29,7 @@ ggplot() +
   xlab('Years of experience') +
   ylab('Salary')
 
-# Visualising the Test set results
+
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
